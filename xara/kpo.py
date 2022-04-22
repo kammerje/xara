74947edd3db9288591638a3e1afca69cc6d210ee
Jens Kammerer <jkammerer@tastrock2021.local>
2022-04-22 10:45:02 -0400

    Adding tutorial notebook, empirical uncertainties step, and CLEARP pupil model

--- xara/kpo.py
+++ xara/kpo.py
@@ -28,8 +28,8 @@ import astropy.io.fits as fits
 from astropy.time import Time
 import copy
 
-import core
-import kpi
+from xara import core
+from xara import kpi
 
 shift = np.fft.fftshift
 fft = np.fft.fft2
@@ -449,8 +449,10 @@ class KPO():
     # =========================================================================
     # =========================================================================
     def extract_KPD_single_frame(self, frame, pscale, cwavel, target=None,
-                                 recenter=False, wrad=None, method="LDFT1"):
-        """Handles the kernel processing of a single square frame
+                                 recenter=False, wrad=None, method="LDFT1",
+                                 algo_cent="BCEN", bmax_cent=None):
+        """ ----------------------------------------------------------------
+        Handles the kernel processing of a single square frame
 
         Parameters
         ----------
@@ -483,7 +485,9 @@ class KPO():
         if recenter is True:
             ysz, xsz = frame.shape
             (x0, y0) = core.determine_origin(img, mask=self.sgmask,
-                                             algo="BCEN", verbose=False)
+                                             algo=algo_cent, verbose=False,
+                                             mykpo=self, m2pix=m2pix,
+                                             bmax=bmax_cent)
             dy, dx = (y0-ysz/2), (x0-xsz/2)
 
         if self.sgmask is not None:  # use apodization mask before extraction
